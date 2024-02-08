ca3b4ca6285445154407c8627bb06c754522dc93
Lawrence ime <lawrenceime246@gmail.com>
2024-02-08 22:14:45 +0100

    I worked on the adding a dark background colour to the nav bar after when scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,26 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [color , setColor] = useState(false);
+
+  const changeColor = () => {
+    if(window.scrollY >= 100){
+      setColor(true);
+    }else{
+      setColor(false);
+    }
+  }
+
+window.addEventListener("scroll" , changeColor);
 
   const handleClick = () => {
     setClick(!click);
   };
 
+
+
   return (
-    <div className="header">
+    <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
