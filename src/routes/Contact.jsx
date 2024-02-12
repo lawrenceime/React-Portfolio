861558b6ed9a46b771473a19bd2a7097d2dc9319
Lawrence ime <lawrenceime246@gmail.com>
2024-02-12 16:46:52 +0100

    I successfully worked on the passing props to the project,about and contact sections

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const Contact = () => {
   return (
     <div>
      <Navbar/>
+     <HeroImg2 heading="CONTACT." text="Let me know how I can be of help"/>
      <Footer/>
     </div>
   )
