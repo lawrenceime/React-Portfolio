93ddbaadcd0ba53d45cce164daf767117a940d23
Lawrence ime <lawrenceime246@gmail.com>
2024-02-14 23:42:44 +0100

    I created cards in the project section and populated them dynamically

---
+++ src/components/WorkData.jsx
@@ -0,0 +1,26 @@
+import HealthImage1 from "../assets/HealthProjectImage.jpg";
+import HealthImage2 from "../assets/HealthProjectImage.jpg";
+import HealthImage3 from "../assets/HealthProjectImage.jpg";
+
+const projectData = [
+  {
+    imgsrc: HealthImage1,
+    title: "Decahealth",
+    text: "Facilitating seamless communication between doctors and patients, streamlining the appointment booking process. The app aims to enhance patient accessibility to medical care and optimize doctor-patient interactions.",
+    view: "https://github.com/lawrenceime/Decahealth",
+  },
+  {
+    imgsrc: HealthImage2,
+    title: "Decahealth",
+    text: "Facilitating seamless communication between doctors and patients, streamlining the appointment booking process. The app aims to enhance patient accessibility to medical care and optimize doctor-patient interactions.",
+    view: "https://github.com/lawrenceime/Decahealth",
+  },
+  {
+    imgsrc: HealthImage3,
+    title: "Decahealth",
+    text: "Facilitating seamless communication between doctors and patients, streamlining the appointment booking process. The app aims to enhance patient accessibility to medical care and optimize doctor-patient interactions.",
+    view: "https://github.com/lawrenceime/Decahealth",
+  },
+];
+
+export default projectData;
